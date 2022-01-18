22d885e948dfc9607585c7688247df978726222c
Tomasz Kostur <tomasz.kostur@gmail.com>
2022-01-18 21:16:06 +0100

    day3

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
-
-pub mod sonar{
+pub mod sonar {
     #[derive(Debug)]
     enum DepthDelta {
         NA,
@@ -52,16 +51,49 @@ pub mod submarine_body {
         }
 
         pub fn print_pos(&self) {
-            println!("Submarine position: distance {}, depth {}", self.horizontal, self.depth);
+            println!(
+                "Submarine position: distance {}, depth {}",
+                self.horizontal, self.depth
+            );
         }
+    }
+    pub fn test() {
+        let mut submarine = Position::new();
+        submarine.down(10);
+        submarine.up(2);
+        submarine.forward(7);
+        submarine.print_pos();
     }
 }
 
+pub mod diagnostics {
+
+    fn calculate_gamma(reaport: Vec<u8>) -> u8 {
+        let mut occurences_of_one: [usize; 5] = [0, 0, 0, 0, 0];
+        for i in reaport.iter() {
+            let jj: [usize; 5] = [0, 1, 2, 3, 4];
+            for j in jj {
+                if (1 << j & i) != 0 {
+                    occurences_of_one[j] += 1;
+                }
+            }
+        }
+        let mut out: u8 = 0;
+        for i in 0..occurences_of_one.len() {
+            if occurences_of_one[i] >= reaport.len()/2 {
+                out += 1 << i;
+            }
+        }
+        out
+    }
+
+    pub fn test() {
+        let input: Vec<u8> = vec![0b10001, 0b01110, 0b00010, 0b00000];
+        let out = calculate_gamma(input);
+        println!("{}", out);
+    }
+}
 
 fn main() {
-    let mut submarine = submarine_body::Position::new();
-    submarine.down(10);
-    submarine.up(2);
-    submarine.forward(7);
-    submarine.print_pos();
+    diagnostics::test();
 }
